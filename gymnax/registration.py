9b1a47aafbd039d90a29190a2a128ebfd7ca98ee
Ziksby <batsyziki@gmail.com>
2023-09-01 14:23:13 +0200

    feat: added new empty room

--- gymnax/registration.py
+++ gymnax/registration.py
@@ -24,6 +24,7 @@ from .environments import (
     Reacher,
     Swimmer,
     Pong,
+    Empty
 )
 
 # =============================================================================
@@ -92,6 +93,8 @@ def make(env_id: str, **env_kwargs):
         env = Swimmer(**env_kwargs)
     elif env_id == "Pong-misc":
         env = Pong(**env_kwargs)
+    elif env_id == "Empty-misc":
+        env = Empty(**env_kwargs)
     else:
         raise ValueError("Environment ID is not registered.")
 
@@ -125,4 +128,5 @@ registered_envs = [
     "Reacher-misc",
     "Swimmer-misc",
     "Pong-misc",
+    "Empty-misc"
 ]
